91b446d7f4c38265e5b8c608859aa2df647c6b76
Aaron Rohrbacher and Margaret Berry <aaron@>
2017-08-22 16:48:50 -0700

    add BankAccount function and variables

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function BankAccount (name, initDeposit) {
+  this.name = name;
+  this.initDeposit = initDeposit;
+}
+
+BankAccount.prototype.withdrawl = function(add) {
+  this.initDeposit += add;
+}
+
+BankAccount.prototype.deposit = function(sub) {
+  this.initDeposit += sub;
+}
+
+$(document).ready(function() {
+  $('#bank').submit(function(event) {
+    event.preventDefault();
+
+    var name = $('input#name').val();
+    var initDeposit = $('input#initDeposit').val();
+    var sub = $('input#withdrawl').val();
+    var add = $('input#deposit').val();
+
+    var newAccount = new BankAccount(name, initDeposit);
+
+    $("#"
+    )
+  });
+});
